44a760b53793067ffa1df5ec7a51032215f35c1b
Eunbit Kim <eunbitkim90@gmail.com>
2018-07-12 15:45:20 +0100

    done 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,4 +4,18 @@ class DockingStation
  def release_bike
     Bike.new
  end
+
+ def dock(bike)
+ # use an instance variable to store the Bike
+ # in the 'state' of this instance
+   @bike = bike
+ end
+
+attr_reader :bike
+
+ #longer way
+ #def bike
+  #(we need to return the bike we dock)
+ #@bike
+ #end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,21 @@ require "docking_station"
 
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it { is_expected.to respond_to(:bike)}
+
+it 'returns docked bikes' do
+  bike = Bike.new
+  subject.dock(bike)
+  # we need to return the bike we just docked
+  expect(subject.bike).to eq bike
+end
+end
 
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
+end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
-  end
+
 end
